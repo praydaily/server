a999ab2d3024b92d63f7cbb34144203204c0ee1e
Julius Härtl <jus@bitgrid.net>
2020-10-07 12:32:16 +0200

    Show unique displayname context in the user share list entries
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- apps/files_sharing/lib/Controller/ShareAPIController.php
+++ apps/files_sharing/lib/Controller/ShareAPIController.php
@@ -229,6 +229,7 @@ class ShareAPIController extends OCSController {
 			$sharedWith = $this->userManager->get($share->getSharedWith());
 			$result['share_with'] = $share->getSharedWith();
 			$result['share_with_displayname'] = $sharedWith !== null ? $sharedWith->getDisplayName() : $share->getSharedWith();
+			$result['share_with_displayname_unique'] = $sharedWith->getEMailAddress() && $sharedWith->getEMailAddress() !== '' ? $sharedWith->getEMailAddress() : $sharedWith->getUID();
 			$result['status'] = [];
 
 			$userStatuses = $this->userStatusManager->getUserStatuses([$share->getSharedWith()]);
